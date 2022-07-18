653c5f4274d74c172d3d915796f63fa135cbde00
tokizuoh <tokizuoh@gmail.com>
2022-07-18 23:22:29 +0900

    init

---
+++ contrail2/contrail2App.swift
@@ -0,0 +1,17 @@
+//
+//  contrail2App.swift
+//  contrail2
+//
+//  Created by tokizo on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct contrail2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
